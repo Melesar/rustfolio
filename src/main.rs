1dfe5ab4e2081ed44891e7254b9111ed8ad5116c
Sergey Pikhota <sergey.pikhota@protonmail.com>
2021-12-06 14:42:09 +0100

    Change --add into a subcommand

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,35 @@ mod add_interactively;
 mod show;
 mod files;
 
-use clap::{App, Arg, SubCommand, crate_version, crate_name, crate_authors, crate_description};
+use std::path::PathBuf;
+use clap::{App, Arg, SubCommand, ArgMatches};
 
 fn main() {
-    let app_config = App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(Arg::with_name("add")
-             .short("a")
-             .long("add")
-             .help("Adds a new entry to a portfolio"))
-        .arg(Arg::with_name("file")
-             .short("f")
-             .long("file")
-             .help("Portfolio file")
-             .value_name("FILE")
-             .takes_value(true)
-             .required(false))
+    let file_arg = Arg::with_name("file")
+        .short("f")
+        .long("file")
+        .help("Portfolio file")
+        .value_name("FILE")
+        .takes_value(true)
+        .required(false);
+
+    let app_config = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .about(clap::crate_description!())
+        .arg(file_arg.clone())
+        .subcommand(SubCommand::with_name("add")
+             .about("Adds a new entry to a portfolio")
+             .arg(file_arg.clone()))
         .get_matches();
 
-    let file_name = app_config.value_of("file").map(|s| s.to_string());
-
-    let file_path = file_name
-            .and_then(|f| files::get_full_path(f).map_or_else(
-                |e| { eprintln!("Failed to handle file name: {}", e); None},
-                |mut f| { f.set_extension("csv"); Some(f) }));
 
     //TODO handle non-tty stdin
-    let result = if app_config.is_present("add") {
+    let result = if let Some(add_matches) = app_config.subcommand_matches("add") {
+        let file_path = get_file_name(add_matches);
         add_interactively::add(file_path)
     } else {
+        let file_path = get_file_name(&app_config);
         let r = portfolio::get_portfolio_interactively(file_path);
         r.and_then(|p| { 
             if let Some(portfolio) = p {
@@ -51,5 +49,13 @@ fn main() {
         Ok(()) => (),
         Err(e) => eprintln!("{}", e),
     };
+}
+
+fn get_file_name(matches: &ArgMatches) -> Option<PathBuf> {
+    let file_name = matches.value_of("file").map(|s| s.to_string());
+
+    file_name.and_then(|f| files::get_full_path(f).map_or_else(
+                |e| { eprintln!("Failed to handle file name: {}", e); None},
+                |mut f| { f.set_extension("csv"); Some(f) }))
 }
 
